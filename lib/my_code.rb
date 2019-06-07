df3a18c50a1d81e564ca7c9ccfd8bd6828264ba6
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-07 19:32:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(arr)
+  i = 0
+  new = []
+  while i < arr.length 
+    new.push(yield(arr[i]))
+    i += 1
+  end
+   return new
+end
+
+def reduce(arr, start = 0)
+    i = 0
+    if arr[i].class == start.class
+      result = start
+    else
+      result = arr [i]
+      i = 1
+    end
+  while i < arr.length 
+    result =  yield(result, arr[i])
+    i += 1
+  end
+  return result
+end
